f972e8be987fd6e5bbf3ff5ba3fef64a7cd9d02e
Iago Floriano <iagomfloriano@gmail.com>
2023-10-22 23:13:23 -0300

    Usa valores passados pela linha de comando

--- client.py
+++ client.py
@@ -1,14 +1,29 @@
+import sys
 import socket
+
+if (len(sys.argv) != 3):
+    print("Quantidade incorreta de argumentos, forma de uso correta:")
+    print(sys.argv[0] + " <ip server> <porta server> <porta cliente>")
+    quit()
+
+server_ip = sys.argv[1]
+
+try:
+    server_port = int(sys.argv[2])
+except Exception as e:
+    print("Porta deve ser um inteiro, forma de uso correta:")
+    print(sys.argv[0] + " <ip server> <porta server> <porta cliente>")
+    quit()
 
 BUFF = 65536
 client_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client_sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF)
 host_name = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
-print('IP:',host_ip)
+print('IP:',(server_ip,server_port))
 port = 9999
 
-client_sock.sendto(b'oi',(host_ip,port))
+client_sock.sendto(b'oi',(server_ip,server_port))
 
 while True:
     packet,_ = client_sock.recvfrom(BUFF)
--- server.py
+++ server.py
@@ -1,5 +1,25 @@
+import sys
 import socket
 import time
+
+if (len(sys.argv) != 3):
+    print("Quantidade incorreta de argumentos, forma de uso correta:")
+    print(sys.argv[0] + " <porta> <delay entre mensagens (segundos)>")
+    quit()
+
+try:
+    port = int(sys.argv[1])
+except Exception as e:
+    print("Porta deve ser um inteiro, forma de uso correta:")
+    print(sys.argv[0] + " <porta> <delay entre mensagens (segundos)>")
+    quit()
+
+try:
+    delay = float(sys.argv[2])
+except:
+    print("Delay entre mensagens deve ser um número real, forma de uso correta:")
+    print(sys.argv[0] + " <porta> <delay entre mensagens (segundos)>")
+    quit()
 
 BUFF = 65536
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -8,32 +28,31 @@ sock.settimeout(0.2)
 host_name = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
 print("IP: ", host_ip)
-port = 9999
 sock_addr = (host_ip,port)
 sock.bind(sock_addr)
 print('ouvindo ',sock_addr)
 
-pacotes_recebidos = 0
+pacotes_enviados = 0
 client_list = []
 
 while True:
-	msg = input()
-	for addr in client_list:
-		try:
-			sock.sendto(msg.encode(), addr)
-		except Exception as e:
-			#falhou em enviar msg
-			pass
+    for addr in client_list:
+        try:
+            sock.sendto(str(pacotes_enviados).encode(), addr)
+        except Exception as e:
+            #falhou em enviar msg
+            pass
+    pacotes_enviados += 1
 
-	while True:
-		try:
-			client_msg,addr = sock.recvfrom(BUFF)
-			if client_msg == b'oi':
-				print('Novo cliente: ',addr)
-				client_list.append(addr)
-			# if client_msg == b'cabou':
-			# if client_msg == b'ack':
-			# 	pacotes_recebidos+=1
-		except:
-			break
-	time.sleep(0.1)
+    while True:
+        try:
+            client_msg,addr = sock.recvfrom(BUFF)
+            if client_msg == b'oi':
+                print('Novo cliente: ',addr)
+                client_list.append(addr)
+            # if client_msg == b'cabou':
+            # if client_msg == b'ack':
+            # 	pacotes_recebidos+=1
+        except:
+            break
+    time.sleep(delay)
